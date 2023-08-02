a579abc8e1a3d7e2d8f895da4b094669297452cb
jeff-yc-wong <jeff33wong@gmail.com>
2023-08-01 14:03:12 -1000

    resolveBasePath returns static url when called

--- src/utils/resloveBasePath.ts
+++ src/utils/resloveBasePath.ts
@@ -1,6 +1,7 @@
 export const resolveBasePath = () => {
-  console.log(window.location.href);
-  let baseUrl = window.location.href.replace('/tapis-ui/', '').split('#')[0];
+  let baseUrl = 'https://dev.develop.tapis.io';
+  //console.log(window.location.href);
+  // let baseUrl = window.location.href.replace('/tapis-ui/', '').split('#')[0];
   // .replace(/^https:\/\/ui\./, 'https://');
   // Direct request from local dev env to dev.develop
   if (/127\.0\.0\.1|localhost|0\.0\.0\.0/.test(baseUrl)) {
