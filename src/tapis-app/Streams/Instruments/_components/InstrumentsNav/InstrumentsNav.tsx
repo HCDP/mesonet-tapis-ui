9e29441fc12fdefd664a10e7c369503658557d6c
jeff-yc-wong <42687471+jeff-yc-wong@users.noreply.github.com>
2023-08-08 13:00:22 -1000

    adding search bar to sitenav, remove other services from sidebar, added station name to site list,  included station name when viewing instruments

--- src/tapis-app/Streams/Instruments/_components/InstrumentsNav/InstrumentsNav.tsx
+++ src/tapis-app/Streams/Instruments/_components/InstrumentsNav/InstrumentsNav.tsx
@@ -17,9 +17,11 @@ const InstrumentsNav: React.FC<{ projectId: string; siteId: string }> = ({
     siteId,
   });
   const definitions: Array<Streams.Instrument> = data?.result ?? [];
-
+  const first_site = definitions[0];
+  const site_name = first_site?.inst_description?.split(" ")[2].split("_")[1];
   return (
     <QueryWrapper isLoading={isLoading} error={error}>
+      {`${site_name}`}
       <Navbar>
         {definitions.length ? (
           definitions.map((instrument) => {
--- src/tapis-app/Streams/Sites/_components/SitesNav/SitesNav.tsx
+++ src/tapis-app/Streams/Sites/_components/SitesNav/SitesNav.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import { useList } from 'tapis-hooks/streams/sites';
 import { Streams } from '@tapis/tapis-typescript';
 import { Navbar, NavItem } from 'tapis-ui/_wrappers/Navbar';
 import { QueryWrapper } from 'tapis-ui/_wrappers';
 import { joinPath } from 'utils/URLManager';
+import styles from './SiteNav.module.scss';
 
 const SitesNav: React.FC<{ projectId: string }> = ({ projectId }) => {
   const { url } = useRouteMatch();
@@ -12,20 +13,73 @@ const SitesNav: React.FC<{ projectId: string }> = ({ projectId }) => {
     projectId,
   });
   const definitions: Array<Streams.Site> = data?.result ?? [];
+  const sorted_definitions: Array<Streams.Site> = definitions.slice().sort((a, b) => {
+    if (a.site_name === undefined && b.site_name === undefined) {
+      return 0;
+    }
+    if (a.site_name === undefined) {
+      return 1;
+    }
+    if (b.site_name === undefined) {
+      return -1;
+    }
+    
+    return a.site_name.localeCompare(b.site_name);});
+
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [filterDefinitions, setFilteredDefinitions] = useState<Array<Streams.Site>>([]);
+
+  const createSearch = (event: any) => {
+    let value = event.target.value;
+    setSearchInput(value);
+  };
+
+  useEffect(() => {
+    let filteredSites = sorted_definitions;
+    
+    if(searchInput !== '') {
+        filteredSites = sorted_definitions.filter(site =>
+        site.site_id?.toLowerCase().startsWith(searchInput.toLowerCase())
+        || (site as any).metadata?.station_name?.toLowerCase().startsWith(searchInput.toLowerCase())
+      );
+    }
+    
+    setFilteredDefinitions(filteredSites);
+  // eslint-disable-next-line
+  }, [searchInput]);
 
   return (
     <QueryWrapper isLoading={isLoading} error={error}>
+      <div className={styles['control']}>
+        Search
+        <i className="icon-search"></i>
+        <div>
+          <input
+            className={styles['string-input']}
+            type="string"
+            value={searchInput}
+            onChange={createSearch}
+          />
+        </div>
+      </div>
       <Navbar>
-        {definitions.length ? (
-          definitions.map((site) => {
+        {filterDefinitions.length ? (
+          filterDefinitions.map((site) => {
             const path = joinPath([url, site.site_name!]);
             return (
               <NavItem to={path} icon="project" key={site.site_name}>
-                {`${site.site_name}`}
+                {`${site.site_name} (${(site as any).metadata?.station_name ?? ""})`}
               </NavItem>
             );
           })
-        ) : (
+        ) : sorted_definitions.length ? sorted_definitions.map((site) => {
+            const path = joinPath([url, site.site_name!]);
+            return (
+              <NavItem to={path} icon="project" key={site.site_name}>
+                {`${site.site_name} (${(site as any).metadata?.station_name ?? ""})`}
+              </NavItem>
+            );
+          }) : (
           <i>No sites found</i>
         )}
       </Navbar>
